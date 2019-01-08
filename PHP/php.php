6844116c1c46173f811ff31fc84a7213904ae5d9
Oxylabs.io <sales@oxylabs.io>
2019-01-08 14:11:07 +0200

    Update php.php

--- PHP/php.php
+++ PHP/php.php
@@ -1,6 +1,6 @@
 <?php
  $curl = curl_init('https://ifconfig.co/ip');
- curl_setopt($curl, CURLOPT_PROXY, '1.1.1.1:60000');
+ curl_setopt($curl, CURLOPT_PROXY, 'PROXY:PORT');
  curl_setopt($curl, CURLOPT_PROXYUSERPWD, 'username:pass');
  curl_exec($curl);
  ?>
