58b6089d53bf82193b54210511c74af12e34fc16
Oxylabs.io <sales@oxylabs.io>
2019-01-08 14:09:39 +0200

    Update node.js

--- Nodejs/node.js
+++ Nodejs/node.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 require('request-promise')({
    url: 'https://ifconfig.co/ip',
-   proxy: 'http://username:pass@1.1.1.1:60000'
+   proxy: 'http://username:pass@PROXY:PORT'
  }).then(function(data){ console.log(data); }, function(err){ console.error(err); });
