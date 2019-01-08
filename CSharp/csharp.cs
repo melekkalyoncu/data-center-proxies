67fdb028ec7326ca29ce0cf6428e7c4bb5438879
Oxylabs.io <sales@oxylabs.io>
2019-01-08 14:08:28 +0200

    Update csharp.cs

--- CSharp/csharp.cs
+++ CSharp/csharp.cs
@@ -6,7 +6,7 @@ class Example
     static void Main()
     {
         var client = new WebClient();
-        client.Proxy = new WebProxy("1.1.1.1:60000");
+        client.Proxy = new WebProxy("PROXY:PORT");
         client.Proxy.Credentials = new NetworkCredential("username", "pass");
         Console.WriteLine(client.DownloadString("https://ifconfig.co/ip"));
     }
